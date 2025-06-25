5fae0fbc9a7e95e491421155a57c89965fa9b0c6
Korki <44523624+Korkii@users.noreply.github.com>
2025-06-25 18:11:17 +0300

    Update script.py

--- script.py
+++ script.py
@@ -17,21 +17,9 @@ def forwarding(pkt: Any) -> Any:
     if len(pkt.layers()) <= 2:
             return False
 
-    pkt.src = MY_MAC 
+    pkt.src = IF_FROM_MAC 
     return pkt
 
 
-def bridge_ifs(if1: NetworkInterface, if2: NetworkInterface, xfrm12: Callable = None, xfrm21: Callable  = None) -> None:
-    """
-    Apply functions on packets from two interfaces, and decide if to forward to eachother.
-
-    :param if1: The first interface to bridge with
-    :param if2: The second interface to bridge with
-    :param xfrm12: the callable to apply to the packet coming from if1
-    :param xfrm21: the callable to apply to the packet coming from if2
-    """
-    bridge_and_sniff(if1, if2, xfrm12, xfrm21)
-
-
 if __name__ == '__main__':
-    bridge_ifs(IF_FROM, IF_TO, xfrm12=forwarding)
+    bridge_and_sniff(IF_FROM, IF_TO, xfrm12=forwarding)
