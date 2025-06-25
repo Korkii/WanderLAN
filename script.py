f90207e45e48f1ec708560175cbbc40ff75ac6d2
Korkii <oryamoryam1@gmail.com>
2025-06-25 16:53:52 +0300

    add nat features

--- script.py
+++ script.py
@@ -1,14 +1,42 @@
 from scapy.all import *
 from typing import Callable
-
+from random import randint
 
 IF_FROM = "enp0s8"
 IF_TO = "enp0s9"
 THIS_IP = "192.168.56.101"
 THIS_MAC = "08:00:27:3c:d9:98"
+nat_table = {}
+MIN_NAT_PORT = 49152 
+MAX_NAT_PORT = 65535
 
 
-def forwarding(pkt: scapy.layers.l2.Ether) -> scapy.layers.l2.Ether:
+def forward_out(pkt):
+    pkt = forwarding(pkt)
+    if not pkt:
+        return False
+
+    # no port, we can simply return
+    if len(pkt.layers()) == 2:
+        return pkt
+
+    rand_port = randint(MIN_NAT_PORT, MAX_NAT_PORT)
+
+    nat_table[(THIS_IP, rand_port)] = (pkt[1].src, pkt[2].sport)
+    
+
+def forward_in(pkt):
+    entry = nat_table.get((pkt[1].dst, pkt[2].dport))
+    if not entry:
+        return False
+
+    pkt[1].dst = entry[0]
+    pkt[2].dport = entry[1]
+
+    return pkt
+
+
+def forwarding(pkt):
     """
     A wrapper function for forwarded packets
 
@@ -35,4 +63,4 @@ def bridge_ifs(if1: NetworkInterface, if2: NetworkInterface, xfrm12: Callable = 
 
 
 if __name__ == '__main__':
-    bridge_ifs(IF_FROM, IF_TO, xfrm12=forwarding)
+    bridge_ifs(IF_FROM, IF_TO, xfrm12=forward_out, xfrm21=forward_in)
