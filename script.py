d287569e11ea2f24e123851fc969d73fc7d5586e
Korkii <oryamoryam1@gmail.com>
2025-06-25 17:28:47 +0300

    fix arp issues and icmp

--- script.py
+++ script.py
@@ -12,20 +12,41 @@ MAX_NAT_PORT = 65535
 
 
 def forward_out(pkt):
-    pkt = forwarding(pkt)
-    if not pkt:
+    """
+    A wrapper function for forwarded packets
+
+    :param pkt: The packet to be forwarded
+    :return: The packet to be forwarded ( changed / unchanged )
+    """
+    print(f"Layers: {pkt.layers()}")
+    if len(pkt.layers()) == 1:
         return False
 
+    pkt.src = THIS_MAC
+    
     # no port, we can simply return
-    if len(pkt.layers()) == 2:
-        return pkt
+    if len(pkt.layers()) == 2 or pkt.layers()[1] == scapy.layers.l2.ARP:
+        return False
+
+    if pkt.layers()[2] == scapy.layers.inet.ICMP:
+        return False
 
     rand_port = randint(MIN_NAT_PORT, MAX_NAT_PORT)
 
     nat_table[(THIS_IP, rand_port)] = (pkt[1].src, pkt[2].sport)
-    
+     
+    pkt[1].src = THIS_IP
+    pkt[2].sport = rand_port
+    return pkt
 
 def forward_in(pkt):
+    """
+    A wrapper function for incoming packets
+
+    :param pkt: The packet to be forwarded inside
+    :return: The packet for be forwarded inside ( changed )
+    """
+    print(f"Layers: {pkt.layers()}")
     entry = nat_table.get((pkt[1].dst, pkt[2].dport))
     if not entry:
         return False
@@ -33,19 +54,6 @@ def forward_in(pkt):
     pkt[1].dst = entry[0]
     pkt[2].dport = entry[1]
     del nat_table[(pkt[1].dst, pkt[2].dport)]
-    return pkt
-
-
-def forwarding(pkt):
-    """
-    A wrapper function for forwarded packets
-
-    :param pkt: The packet to be forwarded
-    :return: The packet to be forwarded ( changed / unchanged )
-    """
-    pkt.src = THIS_MAC
-    if len(pkt.layers()) > 0:
-        pkt[1].src = THIS_IP
     return pkt
 
 
